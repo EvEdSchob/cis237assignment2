cba854b6e146949de8b490583b31776326bb2bf8
Evan Schober <evedschob@live.com>
2017-02-07 22:27:07 -0500

    Complete TransposeMaze method Add PrintMaze method Modify MazeSolver.cs to use it Modify mazeTraversal to solve maze

--- cis237assignment2/MazeSolver.cs
+++ cis237assignment2/MazeSolver.cs
@@ -6,54 +6,125 @@ using System.Threading.Tasks;
 
 namespace cis237assignment2
 {
-    /// <summary>
-    /// This class is used for solving a char array maze.
-    /// You might want to add other methods to help you out.
-    /// A print maze method would be very useful, and probably neccessary to print the solution.
-    /// If you are real ambitious, you could make a seperate class to handle that.
-    /// </summary>
+    //This class is used for solving a char array maze.
+    //You might want to add other methods to help you out.
+    //A print maze method would be very useful, and probably neccessary to print the solution.
+    //If you are real ambitious, you could make a seperate class to handle that.
     class MazeSolver
     {
-        /// <summary>
-        /// Class level memeber variable for the mazesolver class
-        /// </summary>
+        //Class level memeber variable for the mazesolver class
         char[,] maze;
         int xStart;
         int yStart;
+        int width;
+        int height;
 
-        /// <summary>
-        /// Default Constuctor to setup a new maze solver.
-        /// </summary>
+        //Default Constuctor to setup a new maze solver.
         public MazeSolver()
         {}
 
 
-        /// <summary>
-        /// This is the public method that will allow someone to use this class to solve the maze.
-        /// Feel free to change the return type, or add more parameters if you like, but it can be done
-        /// exactly as it is here without adding anything other than code in the body.
-        /// </summary>
+        //This is the public method that will allow someone to use this class to solve the maze.
+        //Feel free to change the return type, or add more parameters if you like, but it can be done
+        //exactly as it is here without adding anything other than code in the body.
         public void SolveMaze(char[,] maze, int xStart, int yStart)
         {
+
             //Assign passed in variables to the class level ones. It was not done in the constuctor so that
             //a new maze could be passed in to this solve method without having to create a new instance.
             //The variables are assigned so they can be used anywhere they are needed within this class. 
             this.maze = maze;
             this.xStart = xStart;
             this.yStart = yStart;
+            //Assign maze dimentions to class level variable for traversal and printing.
+            width = maze.GetLength(0);
+            height = maze.GetLength(1);
 
             //Do work needed to use mazeTraversal recursive call and solve the maze.
+            mazeTraversal(maze, xStart, yStart);
         }
 
-
-        /// <summary>
-        /// This should be the recursive method that gets called to solve the maze.
-        /// Feel free to change the return type if you like, or pass in parameters that you might need.
-        /// This is only a very small starting point.
-        /// </summary>
-        private void mazeTraversal()
+        //This should be the recursive method that gets called to solve the maze.
+        //Feel free to change the return type if you like, or pass in parameters that you might need.
+        //This is only a very small starting point.
+        private void mazeTraversal(char[,] maze, int xPos, int yPos)
         {
+
+            ////First Attempt
+            ////Solves maze but cannot exit recrusive call. Goes outside the bounds of the array.
+            //if ( 0 < xPos && xPos < width && 0 < yPos && yPos < height )
+            //{
+            //    maze[xPos, yPos] = 'X';
+            //    printMaze();
+            //    //Right
+            //    if (maze[xPos + 1, yPos] == '.')
+            //    {
+            //        mazeTraversal(maze, xPos + 1, yPos);
+            //    }
+            //    //Down
+            //    if (maze[xPos, yPos + 1] == '.')
+            //    {
+            //        mazeTraversal(maze, xPos, yPos + 1);
+            //    }
+            //    //Left
+            //    if (maze[xPos - 1, yPos] == '.')
+            //    {
+            //        mazeTraversal(maze, xPos - 1, yPos);
+            //    }
+            //    //Up
+            //    if (maze[xPos, yPos - 1] == '.')
+            //    {
+            //        mazeTraversal(maze, xPos, yPos - 1);
+            //    }
+            //    maze[xPos, yPos] = 'O';
+            //    printMaze();
+            //}
+            //else
+            //{
+            //    printMaze();
+            //}
+
             //Implement maze traversal recursive call
+
+            //Second attempt
+            //Also solves maze but is unable to return
+            if (maze[xPos, yPos] == '.')
+            {
+                maze[xPos, yPos] = 'x';
+                printMaze();
+                if(0 < xPos && xPos < width && 0 < yPos && yPos < height)
+                {
+                    //Right
+                    mazeTraversal(maze, xPos + 1, yPos);
+                    //Down
+                    mazeTraversal(maze, xPos, yPos + 1);
+                    //Left
+                    mazeTraversal(maze, xPos - 1, yPos);
+                    //Up
+                    mazeTraversal(maze, xPos, yPos - 1);
+                    //Back Up
+                    maze[xPos, yPos] = 'O';
+                }
+            }
+
+            //Third Attempt
+        }
+
+        private void printMaze()
+        {
+            //Print the maze
+            int x = 0;
+            int y;
+            for (y = 0; y < height; y++)
+            {
+                if (x == width)
+                    Console.Write(Environment.NewLine);
+                for (x = 0; x < width; x++)
+                {
+                    Console.Write(maze[x, y] + " ");
+                }
+            }
+            Console.WriteLine(Environment.NewLine + Environment.NewLine);
         }
     }
 }
--- cis237assignment2/Program.cs
+++ cis237assignment2/Program.cs
@@ -16,17 +16,16 @@ namespace cis237assignment2
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            /// <summary>
-            /// Starting Coordinates.
-            /// </summary>
+            //Set console buffer height to see all printed mazes.
+            Console.BufferHeight = 5000;
+
+            //Starting Coordinates.
             const int X_START = 1;
             const int Y_START = 1;
 
-            ///<summary>
-            /// The first maze that needs to be solved.
-            /// Note: You may want to make a smaller version to test and debug with.
-            /// You don't have to, but it might make your life easier.
-            /// </summary>
+            //The first maze that needs to be solved.
+            //Note: You may want to make a smaller version to test and debug with.
+            //You don't have to, but it might make your life easier.
             char[,] maze1 = 
             { { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
             { '#', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '#' },
@@ -41,44 +40,46 @@ namespace cis237assignment2
             { '#', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.', '#' },
             { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' } };
 
-            /// <summary>
-            /// Create a new instance of a mazeSolver.
-            /// </summary>
+
+            //Create a new instance of a mazeSolver.
             MazeSolver mazeSolver = new MazeSolver();
+
+            //Tell the instance to solve the first maze with the passed maze, and start coordinates.
+            mazeSolver.SolveMaze(maze1, X_START, Y_START);
 
             //Create the second maze by transposing the first maze
             char[,] maze2 = transposeMaze(maze1);
-
-            /// <summary>
-            /// Tell the instance to solve the first maze with the passed maze, and start coordinates.
-            /// </summary>
-            mazeSolver.SolveMaze(maze1, X_START, Y_START);
 
             //Solve the transposed maze.
             mazeSolver.SolveMaze(maze2, X_START, Y_START);
 
         }
 
-        /// <summary>
-        /// This method will take in a 2 dimensional char array and return
-        /// a char array maze that is flipped along the diagonal, or in mathematical
-        /// terms, transposed.
-        /// ie. if the array looks like 1, 2, 3
-        ///                             4, 5, 6
-        ///                             7, 8, 9
-        /// The returned result will be:
-        ///                             1, 4, 7
-        ///                             2, 5, 8
-        ///                             3, 6, 9
-        /// The current return statement is just a placeholder so the program
-        /// doesn't complain about no return value.
-        /// </summary>
-        /// <param name="mazeToTranspose"></param>
-        /// <returns>transposedMaze</returns>
+        //This method will take in a 2 dimensional char array and return
+        //a char array maze that is flipped along the diagonal, or in mathematical
+        //terms, transposed.
+        //ie. if the array looks like 1, 2, 3
+        //                            4, 5, 6
+        //                            7, 8, 9
+        //The returned result will be:
+        //                            1, 4, 7
+        //                            2, 5, 8
+        //                            3, 6, 9
         static char[,] transposeMaze(char[,] mazeToTranspose)
         {
-            //Write code her to create a transposed maze.
-            return new char[1, 1];
+            int width = mazeToTranspose.GetLength(0);
+            int height = mazeToTranspose.GetLength(1);
+            char[,] transposedMaze = new char[height, width];
+
+            for(int x = 0 ; x < width; x++)
+            {
+                for(int y = 0; y < height; y++)
+                {
+                    transposedMaze[y, x] = mazeToTranspose[x, y];
+                }
+            }
+
+            return transposedMaze;
         }
     }
 }
